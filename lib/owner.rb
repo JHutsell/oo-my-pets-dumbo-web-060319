3e8c2d4797a7e2eee2b52c95bd11f7373b36a0b7
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 20:08:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,6 +32,10 @@ class Owner
     @pets[:dogs].each { |dog| dog.mood = "happy" } 
   end
   
+  def play_with_cats 
+    @pets[:cats].each { |cat| cat.mood = "happy" }
+  end
+  
   
   
   def self.all 
