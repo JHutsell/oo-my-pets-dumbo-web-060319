1d4b68dd0dd20f2ad952069af0ee580e05b56aa0
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 20:03:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,31 @@
 class Owner
   # code goes here
+  attr_reader :species
   
   @@all = []
   
   def initialize(species)
     @species = species 
-    @pets = { 
+    @pets = { fishes: [], cats: [], dogs: [] }
+    
+    @@all << self
+  end
+  
+  def say_species
+    
+  end
+  
+  
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count 
+    @@all.length 
+  end
+  
+  def self.reset_all 
+    @@all = []
   end
 end
